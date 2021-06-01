72a50a5f48e20c6646afe5ab18644efde0f4ef02
Nguyễn Duy Bảo <jayremntb@gmail.com>
2021-06-01 19:29:24 +0700

    add auth route

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use('/alive', authJwt);
 
 
 app.use((req, res, next) => {
-	const error = new Error("Not found");
+	const error = new Error("Route not found");
 	error.status = 404;
 	next(error);
 });
