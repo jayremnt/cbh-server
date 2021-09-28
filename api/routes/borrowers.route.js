91fa8ccb532329e5e001d80e7e8eed98504c6911
Nguyễn Duy Bảo <jayremntb@gmail.com>
2021-09-28 19:19:27 +0700

    change code to id

--- api/routes/borrowers.route.js
+++ api/routes/borrowers.route.js
@@ -7,9 +7,9 @@ const authJwt = require('../middlewares/auth-jwt');
 router.post('/add', authJwt.authenticate, BorrowersController.addBorrower);
 router.post('/all', authJwt.authenticate, BorrowersController.getAllBorrowers);
 router.post('/borrowing', authJwt.authenticate, BorrowersController.getBorrowingBorrowers);
-router.post('/:borrowerCode/', authJwt.authenticate, BorrowersController.getBorrowerInfo);
-router.post('/:borrowerCode/delete', authJwt.authenticate, BorrowersController.deleteBorrower);
-router.post('/:borrowerCode/edit', authJwt.authenticate, BorrowersController.editBorrowerInfo);
-router.post('/:borrowerCode/trace', authJwt.authenticate, BorrowersController.getBorrowerTrace);
+router.post('/:borrowerId/', authJwt.authenticate, BorrowersController.getBorrowerInfo);
+router.post('/:borrowerId/delete', authJwt.authenticate, BorrowersController.deleteBorrower);
+router.post('/:borrowerId/edit', authJwt.authenticate, BorrowersController.editBorrowerInfo);
+router.post('/:borrowerId/trace', authJwt.authenticate, BorrowersController.getBorrowerTrace);
 
 module.exports = router;
